2d8b67af75a0e368b6a96108cc77af2e5a61340e
Juan Prato <pratojuanmanuel2@gmail.com>
2024-10-22 21:57:17 -0300

    create screens files

---
+++ TPCWeb_Grupo21B/Ticket.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPCWeb_Grupo21B.Screens
+{
+    public partial class Ticket : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
